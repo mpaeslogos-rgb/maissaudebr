ed64d5a18e44f38bd05f8da56431bea75263eae9
mpaeslogos-rgb <mpaeslogos@gmail.com>
2026-06-08 17:44:30 -0300

    Add Cypress Cloud projectId to cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'g36mcg',
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.js',
